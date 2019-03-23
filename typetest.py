239907c1e466147ed771b75b941394936759d5c8
Smeppitty <46271620+Smeppitty@users.noreply.github.com>
2019-03-23 14:38:27 -0400

    Update typetest.py

--- typetest.py
+++ typetest.py
@@ -2,7 +2,20 @@ import sys, os
 cwd = os.getcwd()
 sys.path.append(cwd + "/GameEngine/Engine")
 from World import World
+from Entity import Entity
+from Image import Image
+
 
 class TypeTest (World):
     def __init__ (self, engine):
         super().__init__(engine)
+        self.addEntity(FruitNNUT(),50, 50) 
+    def run(self):
+        pass
+
+class FruitNNUT (Entity):
+    def __init__(self):
+        super().__init__()
+        self.image = Image("beckyBlueButton.png")
+        
+    
