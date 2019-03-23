0491506ae436aed7f0c40a29645921af0ee89e50
aaronamk <aaronamk@gmail.com>
2019-03-23 16:54:31 -0400

    Fixed Spacing

--- typetest.py
+++ typetest.py
@@ -5,17 +5,19 @@ from World import World
 from Entity import Entity
 from Image import Image
 
-
 class TypeTest (World):
+
     def __init__ (self, engine):
         super().__init__(engine)
-        self.addEntity(Button(1),50, 50) 
-        self.addEntity(Button(2),450, 50) 
-        self.addEntity(Button(3),850, 50) 
+        self.addEntity(Button(1),50, 50)
+        self.addEntity(Button(2),450, 50)
+        self.addEntity(Button(3),850, 50)
+
     def run(self):
         pass
 
 class Button (Entity):
+  
     def __init__(self, buttonNum):
         super().__init__()
         if buttonNum == 1:
