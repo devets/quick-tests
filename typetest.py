2fc60d8489db31edd7d35f5834ecac0c10aa5b8f
Smep <malynvirth2@gmail.com>
2019-03-23 18:51:05 -0400

    added button pressed alts

--- typetest.py
+++ typetest.py
@@ -1,6 +1,7 @@
 import sys, os
 cwd = os.getcwd()
 sys.path.append(cwd + "/GameEngine/Engine")
+import pymouse
 from World import World
 from Entity import Entity
 from Image import Image
@@ -20,12 +21,31 @@ class Button (Entity):
   
     def __init__(self, buttonNum):
         super().__init__()
+        self.buttonNum = buttonNum
         if buttonNum == 1:
           self.setImage(Image("beckyBlueButton.png"))
         elif buttonNum == 2:
           self.setImage(Image("orangeButton-2.png"))
         else:
           self.setImage(Image("yellowLemonVomitButton.png"))
+        self.mouseDown = False
+        
+    def run(self):
+        self.pushed = False
+        
+        if self.world.mousePressed() and (self.world.mouseX() < self.X+self.width) and
+        (self.world.mouseY() < self.Y+self.height) and (self.world.mouseX() > self.X-self.width) and
+        (self.world.mouseY() >  self.height-self.Y):
+            self.pushed = True
+            if buttonNum == 1:
+                self.setImage(Image("beckyBlueButton-2.png")
+            elif: buttonNum == 2:
+                self.setImage(Image("orangeButton-3.png")
+            elif: button Num == 3:
+                self.setImage(Image("yellowLemonVomitButton-2.png")
+                              
+            
+        else:
+            self.pushed = False
 
-    def pushed_button(self):
-        pass
+        
