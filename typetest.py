08352e72776002f509e3517573f3d9cb4b7b4bcc
Alan Zheng <azheng01@email.wm.edu>
2019-03-23 16:26:40 -0400

    added the buttons to the screen ye

--- typetest.py
+++ typetest.py
@@ -9,13 +9,20 @@ from Image import Image
 class TypeTest (World):
     def __init__ (self, engine):
         super().__init__(engine)
-        self.addEntity(Button(),50, 50) 
+        self.addEntity(Button(1),50, 50) 
+        self.addEntity(Button(2),450, 50) 
+        self.addEntity(Button(3),850, 50) 
     def run(self):
         pass
 
 class Button (Entity):
-    def __init__(self):
+    def __init__(self, buttonNum):
         super().__init__()
-        self.image = Image("beckyBlueButton.png")
+        if buttonNum == 1:
+          self.image = Image("beckyBlueButton.png")
+        elif buttonNum == 2:
+          self.image = Image("orangeButton-2.png")
+        else:
+          self.image = Image("yellowLemonVomitButton.png")
         
     
