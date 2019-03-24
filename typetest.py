4ca5536b0847bf497c1ff72ccb28ca2260e281e6
devets <5199272+devets@users.noreply.github.com>
2019-03-23 21:31:31 -0400

    Made buttons work maybe I guess?

--- typetest.py
+++ typetest.py
@@ -30,25 +30,27 @@ class Button (ActiveEntity):
         self.mouseDown = False
         
     def run(self):
-        self.pushed = False
-        print("haha yes")
-        if self.world.mousePressed() and (self.world.mouseX() < self.x + self.width) and \
-                                         (self.world.mouseY() < self.y + self.height) and \
-                                         (self.world.mouseX() > self.x) and \
-                                         (self.world.mouseY() > self.y):
-            self.pushed = True
-            if self.buttonNum == 1:
-                self.setImage(Image("beckyBlueButton-2.png"))
-                print("BLUE")
-            elif self.buttonNum == 2:
-                self.setImage(Image("orangeButton-2.png"))
-                print("ORANGE")
-            elif self.buttonNum == 3:
-                self.setImage(Image("yellowLemonVomitButton-2.png"))
-                print("YELLOW")
-                              
-            
-        else:
-            self.pushed = False
+        
+        #if self.world.mousePressed():
+        #    print("ENTITY: "+str(self.x)+","+str(self.y)+" - MOUSE: "+str(self.world.mouseX())+","+str(self.world.mouseY()))
+
+
+
+        if self.world.mousePressed() and ((self.world.mouseX() <= self.x + self.width) and \
+                                         (self.world.mouseX() >= self.x) and \
+                                         (self.world.mouseY() <= self.y + self.height) and \
+                                         (self.world.mouseY() >= self.y)):
+                print("It worked")
+                
+                self.pushed = True
+                if self.buttonNum == 1:
+                    self.setImage(Image("beckyBlueButton-2.png"))
+                    print("BLUE")
+                elif self.buttonNum == 2:
+                    self.setImage(Image("orangeButton-2.png"))
+                    print("ORANGE")
+                elif self.buttonNum == 3:
+                    self.setImage(Image("yellowLemonVomitButton-2.png"))
+                    print("YELLOW")
 
         
